20ef51ea2f5d3dfa4ad3aa512b2c8de09c1fdd66
Théo <cobaltium360@gmail.com>
2024-06-13 15:49:35 +0200

    essai n57 pour regler pb allowed host

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -17,8 +17,8 @@ SECRET_KEY = "fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s"
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-# ALLOWED_HOSTS = ["p13.raltheo.fr", "raltheo.fr"]
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = ["*"]
+# ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '').split(',')
 DEBUG = os.environ.get('DJANGO_DEBUG', 'True')
 # DEBUG = False
 
