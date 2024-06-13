754bf64487c5cfb42a51649c0e29eee2fae11004
Théo <cobaltium360@gmail.com>
2024-06-13 14:53:06 +0200

    oublie de quote mdr

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = "fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [p13.raltheo.fr]
+ALLOWED_HOSTS = ["p13.raltheo.fr"]
 
 # DEBUG = False
 
