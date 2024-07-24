8748d7641366a7796de4330cfcba0bf749e60286
raltheo <cobaltium360@gmail.com>
2024-07-24 17:21:28 +0200

    ajout des docstrings sur les test

--- lettings/tests.py
+++ lettings/tests.py
@@ -5,7 +5,13 @@ from .views import lettings_index, letting
 
 
 class LettingViewTests(TestCase):
+    """
+    Tests pour les vues de locations.
+    """
     def setUp(self):
+        """
+        Configure l'adresse et la location de test.
+        """
         self.address = Address.objects.create(
             number=123,
             street="Main St",
@@ -19,15 +25,24 @@ class LettingViewTests(TestCase):
         )
 
     def tearDown(self):
+        """
+        Nettoie la location et l'adresse de test après chaque test.
+        """
         self.letting.delete()
         self.address.delete()
 
     def test_lettings_index_view(self):
+        """
+        Teste que l'index des locations renvoie un statut 200
+        """
         request = RequestFactory().get("/")
         response = lettings_index(request)
         self.assertEqual(response.status_code, 200)
 
     def test_letting_view(self):
+        """
+        Teste qu'une location spécifique renvoie un statut 200
+        """
         request = RequestFactory().get("/")
         response = letting(request, letting_id=self.letting.id)
         self.assertEqual(response.status_code, 200)
--- oc_lettings_site/tests.py
+++ oc_lettings_site/tests.py
@@ -6,6 +6,10 @@ from .models import Address
 
 @pytest.fixture
 def sample_address():
+    """
+    Création une adresse de test
+    L'adresse est nettoyée après le test.
+    """
     address = Address.objects.create(
         number=123,
         street="Main St",
@@ -20,6 +24,9 @@ def sample_address():
 
 @pytest.mark.django_db
 def test_index_view():
+    """
+    Teste que l'index renvoi un statut 200
+    """
     request = RequestFactory().get("/")
     response = index(request)
     assert response.status_code == 200
@@ -27,6 +34,9 @@ def test_index_view():
 
 @pytest.mark.django_db
 def test_custom_404_view():
+    """
+    Teste que la vue custom 404 renvoie un statut 404.
+    """
     request = RequestFactory().get("/nonexistent-page/")
     response = custom_404(request, Exception())
     assert response.status_code == 404
@@ -34,6 +44,9 @@ def test_custom_404_view():
 
 @pytest.mark.django_db
 def test_custom_500_view():
+    """
+    Teste que la vue custom 500 renvoie un statut 500.
+    """
     request = RequestFactory().get("/error-page/")
     response = custom_500(request)
     assert response.status_code == 500
@@ -41,5 +54,9 @@ def test_custom_500_view():
 
 @pytest.mark.django_db
 def test_address_model(sample_address):
+    """
+    Teste le modèle d'adresse.
+    Vérifie que le numéro est bonne et que la representation str de l'adresse est bonne.
+    """
     assert sample_address.number == 123
     assert str(sample_address) == "123 Main St"
