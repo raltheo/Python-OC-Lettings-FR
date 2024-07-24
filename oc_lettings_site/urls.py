c0991658894df00aee1c445e6027de8d47356e52
raltheo <cobaltium360@gmail.com>
2024-07-24 17:38:07 +0200

    separation des routes, chaque app a maintenant un urls.py contenant ses routes

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,17 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from . import views
-from profiles import views as profilesviews
-from lettings import views as lettingsviews
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("trigger-error", views.trigger_500),
-    path("lettings/", lettingsviews.lettings_index, name="lettings_index"),
-    path("lettings/<int:letting_id>/", lettingsviews.letting, name="letting"),
-    path("profiles/", profilesviews.profiles_index, name="profiles_index"),
-    path("profiles/<str:username>/", profilesviews.profile, name="profile"),
+    path('profiles/', include('profiles.urls')),
+    path('lettings/', include('lettings.urls')),
     path("admin/", admin.site.urls),
 ]
 
